1170c42fefa0b41027b4d110cab3d4228121e6f0
dairof7 <dairof7@gmail.com>
2022-07-20 22:33:24 -0500

    [fix] fix convertion round

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,4 +1,4 @@
 List<double> convertToF(List<double> temperaturesInC) {
-  List<double> result = temperaturesInC.map((c) => (((c * 9 / 5) + 32) * 100) / 100).toList();
+  List<double> result = temperaturesInC.map((c) => (((c * 9 / 5) + 32) * 100).round() / 100).toList();
   return result;
 }
