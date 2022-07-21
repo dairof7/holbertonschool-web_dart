5150e33fda093e2a7f5eb9ca80b8254a6d4a6d2a
dairof7 <dairof7@gmail.com>
2022-07-20 22:32:29 -0500

    [fix] fix convertion round

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,19 +1,4 @@
-List<String> convertToF(List<double> tempInC) {
-    
-    var idx = 0;
-    List<String> arr = [];
-   
-    for (; idx < tempInC.length; idx++) { arr.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
-    return arr;
+List<double> convertToF(List<double> temperaturesInC) {
+  List<double> result = temperaturesInC.map((c) => (((c * 9 / 5) + 32) * 100) / 100).toList();
+  return result;
 }
-
-
- // List<double> convertToF(List<double> temperaturesInC) {
- // List<double> arr = temperaturesInC.map((cList) {
-
- // return double.parse((cList * 9 / 5 + 32).toStringAsFixed(2));
- // })
- // .toList();
-
- // return arr;
-// }
